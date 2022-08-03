c6f454b2d0d43072d7f592084aa3a749f249c3e8
Istvan <olivaistvan@gmail.com>
2022-08-02 23:53:46 -0300

    refactor: Erros de sintaxe

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').congif();
+
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -18,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -29,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
